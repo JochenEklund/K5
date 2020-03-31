c4531afd4ba4c5c8a4a1a2cd4d2fd1b813ccd04e
JochenEklund <62107957+JochenEklund@users.noreply.github.com>
2020-03-31 21:46:53 +0200

    Ändrande av bilder
    Lade till script som låter en byta mellan de olika bilderna i varje sektion genom mouseover eller click beroende på skärmens storlek.

---
+++ js/script.js
@@ -0,0 +1,50 @@
+let card = document.querySelectorAll(".Card");
+let art = document.querySelectorAll(".Art");
+let knappN = document.querySelectorAll(".knappNext");
+let knappNph = document.querySelectorAll(".knappNextPlaceholder");
+let knappP = document.querySelectorAll(".knappPre");
+let knappPph = document.querySelectorAll("knappPrePlaceholder");
+let flex = document.querySelectorAll(".flex");
+let storlek = window.matchMedia('(max-width: 600px)');
+
+function ÄndraBild()
+{
+    if(storlek.matches){
+        card.forEach(element => element.removeEventListener("mouseover", Byt));
+        art.forEach(element => element.removeEventListener("mouseout", BytTillbaka));
+        card.forEach(element => element.addEventListener("click", Byt));
+        art.forEach(element => element.addEventListener("click", BytTillbaka));
+    }
+    else{
+        card.forEach(element => element.removeEventListener("click", Byt));
+        art.forEach(element => element.removeEventListener("click", BytTillbaka));
+        card.forEach(element => element.addEventListener("mouseover", Byt));
+        art.forEach(element => element.addEventListener("mouseout", BytTillbaka));
+    }
+}
+
+function Byt()
+{
+    card.forEach(element => element.style.display = "none");
+    art.forEach(element => element.style.display = "initial");
+    knappN.forEach(element => element.style.display = "none");
+    knappNph.forEach(element => element.style.display = "none");
+    knappP.forEach(element => element.style.display = "none");
+    knappPph.forEach(element => element.style.display = "none");
+    flex.forEach(element => element.style.justifyContent = "center");
+}
+
+function BytTillbaka()
+{
+    art.forEach(element => element.style.display = "none");
+    card.forEach(element => element.style.display = "initial");
+    knappN.forEach(element => element.style.display = "initial");
+    knappNph.forEach(element => element.style.display = "initial");
+    knappP.forEach(element => element.style.display = "initial");
+    knappPph.forEach(element => element.style.display = "initial");
+    flex.forEach(element => element.style.justifyContent = "space-between");
+}
+
+ÄndraBild();
+
+storlek.addListener(ÄndraBild);
